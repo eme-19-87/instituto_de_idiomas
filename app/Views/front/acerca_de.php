8031d83861365534c978115e0050b7d0f57c2dea
Valeria Palacios <79058926+ValeSPalacios@users.noreply.github.com>
2023-08-26 04:16:36 -0300

    edit vistas e iconos

--- app/Views/front/acerca_de.php
+++ app/Views/front/acerca_de.php
@@ -1,65 +1,70 @@
-  <header>
-  </header>
-  <div class="historia-instituto">
-  <br>
-  <br>
-  <h1> NUESTRA HISTORIA </h1>
-  <br>
-  <br> 
-    <div class="container">
-  <div class="row">
-    <div class="col-md-6">
-      <img src="assets/img/nuestra_historia.jpg" alt="alumnos" class="img-fluid">
-    </div>
-    <div class="col-md-6">
-    <br>
-      <p style= "text-aling: justify;"> A comienzos de año 2021, los profesores Valeria Palacios y Enrrique Espinoza , decidieron dar comienzo a este proyecto motivados a 
-      satisfacer la demanda existente en base al aprendizaje de diferentes idiomas
-    
-    Ellos a cargo de la institución y de las áreas de inglés y francés, fueron incorporando otras docentes que cubrían
- las materias de alemán e italiano. Gracias al compromiso y responsabilidad en el trabajo, 
-el instituto fue creciendo a pasos agigantados</p>
-<br>
-<p> Nuestro instituto cuenta con un prestigio y una trayectoria académica que avala el éxito de la misma 
- gracias a la confianza y al apoyo de la comunidad educativa.</p>
- <p>
- Contamos con un cuerpo docente altamente capacitado que se adapta constantemente 
- a las exigencias y demandas actuales del alumno y la sociedad.
- </p>
-  
+<header class="historia-instituto">
+  <div>
+    <h1> Nuestra historia </h1>
+    <div class="container-fluid">
+      <div class="row align-items-center">
+        <div class="col-md-5">
+          <img src="assets/img/nuestra_historia.jpg" alt="alumnos" class="img-fluid">
+        </div>
+        <div class="col-md-7">
+          <br>
+          <p style="text-aling: justify;">
+            A comienzos de año 2021, los profesores Valeria Palacios y Enrrique Espinoza , decidieron dar comienzo a este proyecto motivados a 
+            satisfacer la demanda existente en base al aprendizaje de diferentes idiomas
+          
+            Ellos a cargo de la institución y de las áreas de inglés y francés, fueron incorporando otras docentes que cubrían
+            las materias de alemán e italiano. Gracias al compromiso y responsabilidad en el trabajo, 
+            el instituto fue creciendo a pasos agigantados.
+          </p>
+          <br>
+          <p style="text-aling: justify;">
+            Nuestro instituto cuenta con un prestigio y una trayectoria académica que avala el éxito de la misma 
+            gracias a la confianza y al apoyo de la comunidad educativa.
+          </p>
+          <p style="text-aling: justify;">
+            Contamos con un cuerpo docente altamente capacitado que se adapta constantemente 
+            a las exigencias y demandas actuales del alumno y la sociedad.
+          </p>
+      
+        </div>
     </div>
   </div>
-</div> 
-
-<br>
-<div class="container">
-  <div class="row">
-    <div class="col-md-6">
-    <div class="accordion" id="accordionExample">
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-       <strong> Visión </strong>
-      </button>
-    </h2>
-    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-         Nuestra visión es ser una comunidad de aprendizaje dinámica donde los estudiantes se convierten en aprendices de por vida, desarrollan el autoconocimiento y apliquen todo lo aprendido en su vida cotidiana o bien que les sirva como complemento para su perfil profesional.  
+</header>
+<div class="historia-instituto">
+  <div class="container-fluid" style="padding-bottom: 30px">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="accordion" id="accordionExample">
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                <strong> Visión </strong>
+              </button>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+              <div class="accordion-body">
+                Nuestra visión es ser una comunidad de aprendizaje dinámica donde los estudiantes se convierten en aprendices de por vida, desarrollan el autoconocimiento y apliquen todo lo aprendido en su vida cotidiana o bien que les sirva como complemento para su perfil profesional.  
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-      <strong> Misión </strong>
-      </button>
-    </h2>
-    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-      Ayudar a los estudiantes a desarrollar habilidades lingüísticas sólidas ya comunicarse efectivamente en un mundo globalizado. Esto implica proporcionar una educación lingüística de alta calidad, fomentar la comprensión intercultural y ofrecer programas de idiomas innovadores y personalizados que satisfagan las necesidades y objetivos de los estudiantes.
+      <div class="col-md-6">
+        <div class="accordion" id="accordionExample">
+          <div class="accordion-item">
+            <h2 class="accordion-header">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                <strong> Misión </strong>
+              </button>
+            </h2>
+            <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+              <div class="accordion-body">
+                Ayudar a los estudiantes a desarrollar habilidades lingüísticas sólidas ya comunicarse efectivamente en un mundo globalizado. Esto implica proporcionar una educación lingüística de alta calidad, fomentar la comprensión intercultural y ofrecer programas de idiomas innovadores y personalizados que satisfagan las necesidades y objetivos de los estudiantes.
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
     </div>
   </div>
 </div>
--- app/Views/front/quienes_somos.php
+++ app/Views/front/quienes_somos.php
@@ -1,45 +1,37 @@
-<br>
-<div class="cwidth">
-			<h1 style="text-align: center;">Equipo docente</h1>
-		</div>
-<br>
-
-</div>
-<div class="container" style= "background: #5E1A57 " >
-  <div class="row row-cols-3">
-    <div class="col">
-      <div class="card">
-      <img src="assets/img/vale.jpg" class="card-img-top" alt="profe1">
-        <div class="card-body">
-          <h5 class="card-title">Valeria Palacios</h5>
-          <p class="card-text"> Fundadora y docente en ejercicio de nuestro Instituto, 
-          esta profesional se encuentra a cargo de impartir las clases de Alemán e ingles en los diferentes niveles que integran nuestro centro educativo </p>
+<div class="container-fluid py-4 ps-4" style="background: #FFD93D">
+  <h1 style="text-align: left;">Equipo docente</h1>
+  <div class="container-fluid py-4">
+    <div class="row row-cols-3">
+      <div class="col">
+        <div class="card" style="background: #5E1A57; color: white; text-align: justify;">
+          <img src="assets/img/vale.jpg" class="card-img-top" alt="profe1">
+          <div class="card-body">
+            <h5 class="card-title">Valeria Palacios</h5>
+            <p class="card-text"> Docente en ejercicio de nuestro Instituto, 
+            se encuentra a cargo de impartir las clases de Alemán e ingles en los diferentes niveles que integran nuestro centro educativo. </p>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="col">
-      <div class="card">
-      <img src="assets/img/kike.jpg" class="card-img-top" alt="profe2">
-        <div class="card-body">
-          <h5 class="card-title">Enrique Espinoza</h5>
-          <p class="card-text"> Fundador y docente de nuestra querida Institución, este gran profesional se desempeña como Profesor de Italiano desde hace años dentro de nuestro instituto.</p>
+      <div class="col">
+        <div class="card" style="background: #5E1A57; color: white; text-align: justify;">
+          <img src="assets/img/kike.jpg" class="card-img-top" alt="profe2">
+          <div class="card-body">
+            <h5 class="card-title">Enrique Espinoza</h5>
+            <p class="card-text"> Fundador y docente de nuestra querida Institución, este gran profesional se desempeña como Profesor de Italiano desde hace años dentro de nuestro instituto.</p>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="col">
-      <div class="card">
-      <img src="assets/img/jess.jpg" class="card-img-top" alt="profe3">
-        <div class="card-body">
-          <h5 class="card-title">Jesica Queupumil</h5>
-          <p class="card-text"> Profesora de frances, esta integrante del cuerpo docente, se integró hace dos años a nuestra casa de estudios.
-            Actualmente brinda clases dentro de la institución en diferentes niveles.
-          </p>
+      <div class="col">
+        <div class="card" style="background: #5E1A57; color: white; text-align: justify;">
+          <img src="assets/img/jess.jpg" class="card-img-top" alt="profe3">
+          <div class="card-body">
+            <h5 class="card-title">Jesica Queupumil</h5>
+            <p class="card-text"> Profesora de frances, esta profesional se integró hace dos años a nuestra casa de estudios.
+              Actualmente brinda clases dentro de la institución en diferentes niveles.
+            </p>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </div>
-<br>
-<br>
-<br>
-<br>
